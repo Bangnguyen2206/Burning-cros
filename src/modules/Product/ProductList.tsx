6b23085de696018a44305cb2b3f4c3519a5bcf30
Bangnguyen2206 <tuyetbang0304@gmail.com>
2023-06-25 14:31:49 +0700

    Feat: Handle change search products

--- src/modules/Product/ProductList.tsx
+++ src/modules/Product/ProductList.tsx
@@ -1,31 +1,54 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useContext, useState, useEffect, useRef } from 'react'
+import { useContext, useState, useEffect, useRef, useCallback } from 'react'
 import { Stack } from '@mui/system'
 import Grid from '@mui/material/Grid'
 import ProductCard from '@components/ProductCart/ProductCart'
 import { ProductContext } from 'contexts/Product/Product'
-import { ProductItem } from 'constants/constants'
+import { ProductItem, productPrefix } from 'constants/constants'
 import LoadingSpinner from '@components/LoadingSpinner/LoadingSpinner'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import NotFound from '@components/NotFound/NotFound'
+import { isCancel } from 'axios'
+import axios from 'axios'
+import axiosClient from 'helpers/httpClient'
 
 export default function ProductList() {
-  const {
-    products,
-    fetchProductList,
-    isLoading,
-    hasMore,
-    searchProductList,
-  } = useContext(ProductContext)
+  const { products, fetchProductList, isLoading, hasMore } = useContext(
+    ProductContext,
+  )
 
   const [pageNumber, setPageNumber] = useState(0)
   const containerRef = useRef<HTMLDivElement | null>(null)
   const [listProducts, setListProducts] = useState<any>([])
   const [visible, setVisible] = useState(false)
   const [search, setSearch] = useState(false)
+  const [params, setParams] = useState('')
+  const axiosSource = useRef<any>(null)
+
+  const newCancelToken = useCallback(() => {
+    axiosSource.current = axios?.CancelToken?.source()
+    return axiosSource.current.token
+  }, [])
+
+  useEffect(() => {
+    const url = `${productPrefix}products/search?q=${params}`
+    axiosClient
+      .get(url, { cancelToken: newCancelToken() })
+      .then((res: any) => {
+        setListProducts(res.data.products)
+      })
+      .catch((error: string) => {
+        if (axios.isCancel(error)) return
+      })
+    setVisible(true)
+    return () => {
+      axiosSource.current.cancel()
+    }
+  }, [newCancelToken, isCancel, params])
 
   const onIntersection = (entries: any) => {
     const firstEntry = entries[0].isIntersecting
@@ -37,13 +60,15 @@ export default function ProductList() {
 
   const handleChangeSearch = (params: string) => {
     setSearch(true)
-    searchProductList(params)
-      .then((res: any) => {
-        setListProducts(res.products)
-      })
-      .catch((error: string) => {
-        return error
-      })
+    // searchProductList(params)
+    //   .then((res: any) => {
+    //     setListProducts(res.products)
+    //   })
+    //   .catch((error: string) => {
+    //     return error
+    //   })
+    // makeRequestCreator(params)
+    setParams(params)
   }
 
   useEffect(() => {
@@ -69,10 +94,11 @@ export default function ProductList() {
   }
 
   const handleGetMoreProduct = () => {
-    if (visible && !search) {
+    if (!search) {
       fetchProductList(pageNumber)
         .then((res: any) => {
           setVisible(false)
+
           setListProducts([...listProducts, ...res.products])
         })
         .catch((error: string) => {
